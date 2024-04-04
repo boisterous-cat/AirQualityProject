c97e2ecd70b5cff540e5f74d5a8ae3d830627de1
boisterous-cat <93883573+boisterous-cat@users.noreply.github.com>
2024-04-05 01:10:30 +0300

    Major update on Fastapi nd training model

--- Services/FastApi/data_request_model.py
+++ Services/FastApi/data_request_model.py
@@ -1,9 +1,19 @@
-from pydantic import BaseModel, validator, root_validator
+from pydantic import BaseModel, Field, NonNegativeFloat
 from typing import Optional
+from typing import List
 
 
 class MlRequest(BaseModel):
-    co: float
-    no: float
-    ozone: float
-    pm2: float
+    co: Optional[float] = Field(0, ge=0, le=1000)
+    no: Optional[float] = Field(0, ge=0, le=1000)
+    ozone: Optional[float] = Field(0, ge=0, le=1000)
+    pm2: Optional[float] = Field(0, ge=0, le=1000)
+
+
+class MlRequests(BaseModel):
+    objects: List[MlRequest]
+
+
+class UserLocation(BaseModel):
+    Country: str
+    City: str
--- Services/Streamlit/service/data_request_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, validator, root_validator
-from typing import Optional
-
-
-class MlRequest(BaseModel):
-    co: float
-    no: float
-    ozone: float
-    pm2: float
